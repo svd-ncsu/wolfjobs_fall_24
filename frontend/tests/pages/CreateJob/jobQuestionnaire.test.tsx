c8ce31349c1e650c387c70556a95a4c26c11ec3f
Yuvraj-SB <yuvrajbhatia04@gmail.com>
2024-11-01 17:32:11 -0400

    Added 3 new test cases for JobQuestionnaire

--- frontend/tests/pages/CreateJob/jobQuestionnaire.test.tsx
+++ frontend/tests/pages/CreateJob/jobQuestionnaire.test.tsx
@@ -4,11 +4,35 @@ import JobQuestionnaire from "../../../src/Pages/CreateJob/jobQuestionnaire";
 import { MemoryRouter } from "react-router";
 
 describe("JobQuestionnaire", () => {
-  it("renders JobQuestionnaire", () => {
+  it("renders JobQuestionnaire component without crashing", () => {
     render(
       <MemoryRouter>
         <JobQuestionnaire />
       </MemoryRouter>
     );
+    expect(screen.getByText("Create New Job Listing")).toBeInTheDocument();
+  });
+
+  it("contains a Proceed button", () => {
+    render(
+      <MemoryRouter>
+        <JobQuestionnaire />
+      </MemoryRouter>
+    );
+    expect(screen.getByRole("button", { name: /Proceed/i })).toBeInTheDocument();
+  });
+
+  it("contains question input fields", () => {
+    render(
+      <MemoryRouter>
+        <JobQuestionnaire />
+      </MemoryRouter>
+    );
+    expect(screen.getByLabelText(/Question 1/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Question 2/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Question 3/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Question 4/i)).toBeInTheDocument();
   });
 });
+
+
