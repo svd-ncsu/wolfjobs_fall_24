4a25a640f5b3957ef7e8d762a460c882d8d45808
YashChaudhary16 <yashchaudhary.yc16@gmail.com>
2024-10-31 23:33:41 -0400

    Updated user_api file in backend

--- backend/controllers/api/v1/users_api.js
+++ backend/controllers/api/v1/users_api.js
@@ -5,10 +5,85 @@ const History = require("../../../models/history");
 const Job = require("../../../models/job");
 const Application = require("../../../models/application");
 const AuthOtp = require("../../../models/authOtp");
-
 const nodemailer = require("nodemailer");
-
 require("dotenv").config();
+
+module.exports.resetPassword = async function (req, res) {
+  try {
+    const { userId, newPassword } = req.body;
+
+    // Find the user by ID
+    let user = await User.findById(userId);
+
+    if (!user) {
+      return res.json(404, {
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    // Update the user's password
+    user.password = newPassword;
+    await user.save();
+
+    return res.json(200, {
+      message: "Password reset successful",
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json(500, {
+      message: "Internal Server Error",
+    });
+  }
+};
+
+// Get all users (for admin)
+module.exports.getAllUsers = async function (req, res) {
+  try {
+    let users = await User.find({});
+    res.set("Access-Control-Allow-Origin", "*");
+    return res.json(200, {
+      message: "List of all users",
+      data: {
+        users: users,
+      },
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json(500, {
+      message: "Internal Server Error",
+    });
+  }
+};
+
+// Delete a specific user (for admin)
+module.exports.deleteUser = async function (req, res) {
+  try {
+    let userId = req.params.id;
+    let user = await User.findById(userId);
+
+    if (!user) {
+      return res.json(404, {
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    await User.findByIdAndDelete(userId);
+    res.set("Access-Control-Allow-Origin", "*");
+    return res.json(200, {
+      message: "User deleted successfully",
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json(500, {
+      message: "Internal Server Error",
+    });
+  }
+};
 
 module.exports.createSession = async function (req, res) {
   try {
@@ -67,7 +142,7 @@ module.exports.signUp = async function (req, res) {
   try {
     if (req.body.password != req.body.confirm_password) {
       return res.json(422, {
-        message: "Passwords donot match",
+        message: "Passwords do not match",
       });
     }
 
@@ -75,11 +150,9 @@ module.exports.signUp = async function (req, res) {
       if (user) {
         res.set("Access-Control-Allow-Origin", "*");
         return res.json(200, {
-          message: "Sign Up Successful, here is your token, plz keep it safe",
+          message: "Sign Up Successful, here is your token, please keep it safe",
 
           data: {
-            //user.JSON() part gets encrypted
-
             token: jwt.sign(user.toJSON(), "wolfjobs", {
               expiresIn: "100000",
             }),
@@ -97,14 +170,11 @@ module.exports.signUp = async function (req, res) {
             });
           }
 
-          // let userr = User.findOne({ email: req.body.email });
           res.set("Access-Control-Allow-Origin", "*");
           return res.json(200, {
-            message: "Sign Up Successful, here is your token, plz keep it safe",
+            message: "Sign Up Successful, here is your token, please keep it safe",
 
             data: {
-              //user.JSON() part gets encrypted
-
               token: jwt.sign(user.toJSON(), "wolfjobs", {
                 expiresIn: "100000",
               }),
@@ -136,9 +206,6 @@ module.exports.getProfile = async function (req, res) {
       message: "The User info is",
 
       data: {
-        //user.JSON() part gets encrypted
-
-        //token: jwt.sign(user.toJSON(), env.jwt_secret, { expiresIn: "100000" }),
         user: user,
       },
       success: true,
@@ -153,7 +220,6 @@ module.exports.getProfile = async function (req, res) {
 };
 
 module.exports.editProfile = async function (req, res) {
-  // if (req.body.password == req.body.confirm_password) {
   try {
     let user = await User.findById(req.body.id);
 
@@ -165,7 +231,6 @@ module.exports.editProfile = async function (req, res) {
     user.hours = req.body.hours;
     user.availability = req.body.availability;
     user.gender = req.body.gender;
-    // user.dob = req.body.dob;
     check = req.body.skills;
     user.skills = check;
     user.save();
@@ -174,11 +239,6 @@ module.exports.editProfile = async function (req, res) {
       message: "User is updated Successfully",
 
       data: {
-        //user.JSON() part gets encrypted
-
-        // token: jwt.sign(user.toJSON(), env.jwt_secret, {
-        //   expiresIn: "100000",
-        // }),
         user,
       },
       success: true,
@@ -190,12 +250,8 @@ module.exports.editProfile = async function (req, res) {
       message: "Internal Server Error",
     });
   }
-  // } else {
-  //   return res.json(400, {
-  //     message: "Bad Request",
-  //   });
-  // }
 };
+
 module.exports.searchUser = async function (req, res) {
   try {
     var regex = new RegExp(req.params.name, "i");
@@ -206,9 +262,6 @@ module.exports.searchUser = async function (req, res) {
       message: "The list of Searched Users",
 
       data: {
-        //user.JSON() part gets encrypted
-
-        //token: jwt.sign(user.toJSON(), env.jwt_secret, { expiresIn: "100000" }),
         users: users,
       },
       success: true,
@@ -233,9 +286,6 @@ module.exports.getHistory = async function (req, res) {
       message: "The User Profile",
 
       data: {
-        //user.JSON() part gets encrypted
-
-        // token: jwt.sign(user.toJSON(), env.jwt_secret, { expiresIn: "100000" }),
         history: history,
       },
       success: true,
@@ -271,7 +321,6 @@ module.exports.createJob = async function (req, res) {
     return res.json(200, {
       data: {
         job: job,
-        //token: jwt.sign(user.toJSON(), env.jwt_secret, { expiresIn: "100000" })
       },
       message: "Job Created!!",
       success: true,
@@ -288,7 +337,6 @@ module.exports.createJob = async function (req, res) {
 module.exports.index = async function (req, res) {
   let jobs = await Job.find({}).sort("-createdAt");
 
-  //Whenever we want to send back JSON data
   res.set("Access-Control-Allow-Origin", "*");
   return res.json(200, {
     message: "List of jobs",
@@ -300,7 +348,6 @@ module.exports.index = async function (req, res) {
 module.exports.fetchApplication = async function (req, res) {
   let application = await Application.find({}).sort("-createdAt");
 
-  //Whenever we want to send back JSON data
   res.set("Access-Control-Allow-Origin", "*");
   return res.json(200, {
     message: "List of Applications",
@@ -310,9 +357,6 @@ module.exports.fetchApplication = async function (req, res) {
 };
 
 module.exports.createApplication = async function (req, res) {
-  // let user = await User.findOne({ _id: req.body.id });
-  // check = req.body.skills;
-
   try {
     const existingApplication = await Application.findOne({
       applicantid: req.body.applicantId,
@@ -328,35 +372,31 @@ module.exports.createApplication = async function (req, res) {
     }
 
     let application = await Application.create({
-      // applicantemail: req.body.applicantemail,
       applicantid: req.body.applicantid,
       applicantname: req.body.applicantname,
-      applicantemail: req.body.applicantemail,
-      applicantskills: req.body.applicantSkills,
-      skills: req.body.skills,
-      address: req.body.address,
-      phonenumber: req.body.phonenumber,
-      hours: req.body.hours,
-      dob: req.body.dob,
-      gender: req.body.gender,
-      jobname: req.body.jobname,
       jobid: req.body.jobid,
+      applicantans1: req.body.applicantans1,
+      applicantans2: req.body.applicantans2,
+      applicantans3: req.body.applicantans3,
+      applicantans4: req.body.applicantans4,
       managerid: req.body.managerid,
+      managerAffilication: req.body.managerAffilication,
+      skills: req.body.skills,
+      status: "pending",
     });
     res.set("Access-Control-Allow-Origin", "*");
     return res.json(200, {
+      message: "Application Created!!",
       data: {
         application: application,
-        //token: jwt.sign(user.toJSON(), env.jwt_secret, { expiresIn: "100000" })
       },
-      message: "Job Created!!",
       success: true,
     });
   } catch (err) {
     console.log(err);
 
     return res.json(500, {
-      message: "NOT CREATED",
+      message: "Internal Server Error",
     });
   }
 };
@@ -366,22 +406,10 @@ module.exports.modifyApplication = async function (req, res) {
     let application = await Application.findById(req.body.applicationId);
 
     application.status = req.body.status;
-
-    //change answer only from screening to grading
-    if (req.body.status === "grading") {
-      application.answer1 = req.body.answer1;
-      application.answer2 = req.body.answer2;
-      application.answer3 = req.body.answer3;
-      application.answer4 = req.body.answer4;
-    }
-
-    if (req.body.status === "rating") {
-      application.rating = req.body.rating;
-    }
     application.save();
     res.set("Access-Control-Allow-Origin", "*");
     return res.json(200, {
-      message: "Application is updated Successfully",
+      message: "Status of Application Updated!!",
       data: {
         application,
       },
@@ -399,20 +427,12 @@ module.exports.modifyApplication = async function (req, res) {
 module.exports.acceptApplication = async function (req, res) {
   try {
     let application = await Application.findById(req.body.applicationId);
-
-    application.status = "1";
-
+    application.status = "accepted";
     application.save();
     res.set("Access-Control-Allow-Origin", "*");
     return res.json(200, {
-      message: "Application is updated Successfully",
-
+      message: "Status of Application Updated!!",
       data: {
-        //user.JSON() part gets encrypted
-
-        // token: jwt.sign(user.toJSON(), env.jwt_secret, {
-        //   expiresIn: "100000",
-        // }),
         application,
       },
       success: true,
@@ -429,20 +449,12 @@ module.exports.acceptApplication = async function (req, res) {
 module.exports.rejectApplication = async function (req, res) {
   try {
     let application = await Application.findById(req.body.applicationId);
-
-    application.status = "2";
-
+    application.status = "rejected";
     application.save();
     res.set("Access-Control-Allow-Origin", "*");
     return res.json(200, {
-      message: "Application is updated Successfully",
-
+      message: "Status of Application Updated!!",
       data: {
-        //user.JSON() part gets encrypted
-
-        // token: jwt.sign(user.toJSON(), env.jwt_secret, {
-        //   expiresIn: "100000",
-        // }),
         application,
       },
       success: true,
@@ -458,21 +470,13 @@ module.exports.rejectApplication = async function (req, res) {
 
 module.exports.closeJob = async function (req, res) {
   try {
-    let job = await Job.findById(req.body.jobid);
-
-    job.status = "closed";
-
+    let job = await Job.findById(req.body.jobId);
+    job.isOpen = false;
     job.save();
     res.set("Access-Control-Allow-Origin", "*");
     return res.json(200, {
-      message: "Job is updated Successfully",
-
+      message: "Status of Job Updated!!",
       data: {
-        //user.JSON() part gets encrypted
-
-        // token: jwt.sign(user.toJSON(), env.jwt_secret, {
-        //   expiresIn: "100000",
-        // }),
         job,
       },
       success: true,
@@ -496,34 +500,44 @@ function getTransport() {
   });
 }
 
-// Generate OTP ans send email to user
+// Generate OTP and send email to user
 module.exports.generateOtp = async function (req, res) {
-  const otp = Math.floor(100000 + Math.random() * 900000);
   try {
-    let authOtp = await AuthOtp.create({
-      userId: req.body.userId,
-      otp: otp,
-    });
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json(404, {
+        message: "No user found with this email address",
+      });
+    }
 
-    const { email } = await User.findById(req.body.userId);
-    // Send mail to user
-    const mailOptions = {
-      from: '"Job Portal" <' + process.env.EMAIL + ">", // sender address
-      to: email, // list of receivers
-      subject: "OTP", // Subject line
-      html: `<p>Your OTP is ${otp}</p>`, // plain text body
+    let otp = Math.floor(1000 + Math.random() * 9000);
+    let otpRecord = await AuthOtp.findOneAndUpdate(
+      { userId: user._id },
+      { otp: otp, createdAt: new Date() },
+      { upsert: true, new: true }
+    );
+
+    let transporter = getTransport();
+    let mailOptions = {
+      from: process.env.EMAIL,
+      to: user.email,
+      subject: "Your OTP for Verification",
+      text: `Your OTP is ${otp}`,
     };
 
-    await getTransport().sendMail(mailOptions);
-
-    res.set("Access-Control-Allow-Origin", "*");
-    return res.json(200, {
-      success: true,
-      message: "OTP is generated Successfully",
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res.json(500, { message: "Error sending OTP" });
+      } else {
+        return res.json(200, {
+          message: "OTP sent to email",
+          data: { otpId: otpRecord._id },
+        });
+      }
     });
   } catch (err) {
     console.log(err);
-
     return res.json(500, {
       message: "Internal Server Error",
     });
@@ -532,37 +546,33 @@ module.exports.generateOtp = async function (req, res) {
 
 module.exports.verifyOtp = async function (req, res) {
   try {
-    const authOtp = await AuthOtp.findOne({
+    let otpRecord = await AuthOtp.findOne({
       userId: req.body.userId,
       otp: req.body.otp,
     });
 
-    if (!authOtp) {
-      return res.json(422, {
-        error: true,
-        message: "OTP is not correct",
+    if (!otpRecord) {
+      return res.json(400, {
+        message: "Invalid OTP",
       });
     }
 
-    authOtp.remove();
+    let timeDiff = new Date() - otpRecord.createdAt;
+    if (timeDiff > 5 * 60 * 1000) {
+      return res.json(400, {
+        message: "OTP has expired",
+      });
+    }
 
-    await User.updateOne(
-      { _id: req.body.userId },
-      { $set: { isVerified: true } }
-    );
-
-    res.set("Access-Control-Allow-Origin", "*");
+    await AuthOtp.findByIdAndDelete(otpRecord._id);
     return res.json(200, {
+      message: "OTP verified successfully",
       success: true,
-      message: "OTP is verified Successfully",
     });
   } catch (err) {
     console.log(err);
-
     return res.json(500, {
       message: "Internal Server Error",
     });
   }
 };
-
-
