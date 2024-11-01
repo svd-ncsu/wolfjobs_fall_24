762828b5308af4ee03a7cc42bcf624f819e98380
Yuvraj-SB <yuvrajbhatia04@gmail.com>
2024-11-01 16:45:45 -0400

    Updated login page to add testid
    Added some tests for the login page

--- frontend/tests/pages/Auth/LoginPage.test.tsx
+++ frontend/tests/pages/Auth/LoginPage.test.tsx
@@ -4,13 +4,32 @@ import LoginPage from "../../../src/Pages/Auth/LoginPage";
 import { MemoryRouter } from "react-router";
 
 describe("LoginPage", () => {
-  it("renders LoginPage", () => {
+  beforeEach(() => {
     render(
       <MemoryRouter>
         <LoginPage />
       </MemoryRouter>
     );
-    // const headline = screen.getByText(/Hello/i);
-    // expect(headline).toBeInTheDocument();
+  });
+
+  it("renders the Login button", () => {
+    const loginButton = screen.getByRole("button", { name: /login/i });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it("renders the container for the background image", () => {
+    const backgroundContainer = screen.getByTestId("login-background");
+    expect(backgroundContainer).toBeInTheDocument();
+  });
+
+  it("renders the 'Create a new account' option", () => {
+    const createAccountLink = screen.getByText(/create a new account/i);
+    expect(createAccountLink).toBeInTheDocument();
+  });
+
+  it("renders the sign-in form", () => {
+    expect(screen.getByText(/sign in to your account/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/email id/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
   });
 });
