d5bc5c1d63644736b1d52d88d3883095c167b334
Devanshu Shah <dastaryt720@gmail.com>
2024-11-24 17:59:53 -0500

    Updated Database String

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://ranadivevrushali31:v5Wes95M6aNZz341@cluster0.yein4.mongodb.net/');
+mongoose.connect('mongodb+srv://harshchaudhari882:S0wDrxspsjyELBHZ@cluster0.ycsoz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error connecting to mongodb'));
